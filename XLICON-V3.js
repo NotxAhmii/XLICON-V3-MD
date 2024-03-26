12bd036ae6ba0e426dacd62e4518da2c62cf54d2
NotxAhmii <164718999+NotxAhmii@users.noreply.github.com>
2024-03-26 23:39:37 +0500

    Update XLICON-V3.js

--- XLICON-V3.js
+++ XLICON-V3.js
@@ -1,6 +1,6 @@
 //══════════════════════════════════════════════════════════════════════════════════════════════════════//
 //                                                                                                      //
-//                                 𝐗𝐋𝐈𝐂𝐎𝐍-𝐕𝟑-𝐌𝐃    𝐁𝐎𝐓                                                //
+//                                 NotxAhmii-𝐕𝟑_𝐌𝐃    𝐁𝐎𝐓                                                //
 //                                                                                                      //
 //                                         Ｖ：3.0                                                      //
 //                                                                                                      //
@@ -15,22 +15,22 @@
 //                                                                                                      //
 //══════════════════════════════════════════════════════════════════════════════════════════════════════//
 //*
-//  * @project_name : XLICON-V3-MD
-//  * @author : salmanytofficial
-//  * @youtube : https://www.youtube.com/@s4salmanyt
-//   * @description : XLICON-V3 ,A Multi-functional whatsapp user bot.
+//  * @project_name : NotxAhmii-V3-MD
+//  * @author : Ahmad Haseeb
+//  * @youtube : https://www.youtube.com/@notxahmiitech
+//   * @description : NotxAhmii-V3 ,A Multi-functional whatsapp user bot.
 //*
 //*
-//base by DGXeon
+//base by Ahmad Haseeb
 //re-upload? recode? copy code? give credit ya :)
-//Instagram: unicorn_xeon13
-//Telegram: t.me/ahmmitech
-//GitHub: @salmanytofficial
-//WhatsApp: +923184070915
-//want more free bot scripts? subscribe to my youtube channel: https://youtube.com/@DGXeon
-//   * Created By Github: DGXeon.
-//   * Credit To Xeon
-//   * © 2024 XLICON-V3-MD.
+//Instagram: itx.... Me79
+//Facebook: https://www.facebook.com/mianhaseeb.mianhaseeb.583
+//GitHub: @notxahmii
+//WhatsApp: +923334461778
+//want more free bot scripts? subscribe to my youtube channel: https://www.youtube.com/@notxahmiitech
+//   * Created By Github: NotxAhmii.
+//   * Credit To Ahmad Haseeb
+//   * © 2024 NotxAhmii-V3-MD.
 // ⛥┌┤
 // */
 
